835487588a4e50f9724b9d5852d98beeac6527ec
Andrew Rivaldo de Albuquerque Alves <andrewalbuquerquealves@gmail.com>
2022-12-08 16:45:19 -0300

    If

---
+++ Controle/If.java
@@ -0,0 +1,22 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class If {
+    public static void main(String[] args) {
+      Scanner leia = new Scanner(System.in);
+      
+        System.out.print("Informe a media: ");
+        double media = leia.nextDouble();
+
+        if (media >= 7.0) {
+            System.out.print("Aluno Aprovado");
+        }
+        
+        if (media < 7 )
+        {
+            System.out.print("Aluno Reprovado");
+        }
+    
+    }
+}

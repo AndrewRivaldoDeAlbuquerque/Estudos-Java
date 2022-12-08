5fb88f68f254d0aaaa21fed8a554ed0afcdc4f6b
Andrew Rivaldo de Albuquerque Alves <andrewalbuquerquealves@gmail.com>
2022-12-08 08:57:28 -0300

    bhaskara

---
+++ Fundamentos/Bhaskara.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+public class Bhaskara {
+    public static void main(String[] args) {
+      double a = 1;
+      double b = 12;
+      double c = -13;  
+      double d = Math.pow(b, 2);
+      System.out.println(d);
+
+      double delta = d - 4 * a * c;
+
+      System.out.printf("Valor de delta é: %.2f", delta);
+
+    
+
+        
+
+    
+    }   
+}

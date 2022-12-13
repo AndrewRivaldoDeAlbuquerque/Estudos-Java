a122a2125c5652ec576c43a03b376ff7231b5489
Andrew Rivaldo de Albuquerque Alves <andrewalbuquerquealves@gmail.com>
2022-12-13 18:25:15 -0300

    desafio e while

---
+++ Controle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package Controle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 10) {
+           i++;
+            System.out.printf("Bom dia!!");
+            
+        }
+    }
+    
+}
